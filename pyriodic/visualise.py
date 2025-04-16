fe606a04d270a1874eb017ef830ce9410c50b5bb
laurabpaulsen <202005791@post.au.dk>
2025-04-16 13:54:14 +0200

    update density function

--- pyriodic/visualise.py
+++ pyriodic/visualise.py
@@ -2,12 +2,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from circular import Circular
-from scipy.stats import gaussian_kde
-
+from math import pi
 DEFAULT_COLOUR = "forestgreen"
 
+
+
+def vonmises_kde(data, kappa, min_x = -pi, max_x = pi, n_bins=100):
+    from scipy.special import i0
+    bins = np.linspace(min_x, max_x, n_bins)
+    x = np.linspace(min_x, max_x, n_bins)
+    # integrate vonmises kernels
+    kde = np.exp(kappa*np.cos(x[:, None]-data[None, :])).sum(1)/(2*np.pi*i0(kappa))
+    kde /= np.trapz(kde, x=bins)
+    return bins, kde
+
 class PyCircPlot:
-    def __init__(self, circ: Circular, fig_size=(6, 6), dpi=300, ax=None, zero_pos = "W", ylim = None):
+    def __init__(self, circ: Circular, fig_size=(6, 6), dpi=300, ax=None, ylim = None):
         self.circ = circ
 
         if ax is None:
@@ -21,9 +31,9 @@ class PyCircPlot:
             self.ax = ax
             self.fig = ax.figure
 
-        self.prepare_ax(zero_pos = zero_pos.upper(), ylim = ylim)
+        self.prepare_ax(ylim = ylim)
 
-    def prepare_ax(self, zero_pos, ylim):
+    def prepare_ax(self, ylim):
         # Remove radial ticks
         self.ax.set_yticklabels([])
         self.ax.set_yticks([])
@@ -38,38 +48,41 @@ class PyCircPlot:
         if ylim:
             self.ax.set_ylim(*ylim)
 
-
-        # Set theta zero location
-        self.ax.set_theta_zero_location(zero_pos)
+        # Set theta offset
+        zero_location = pi if self.circ.zero == "pi" else self.circ.zero
+        self.ax.set_theta_offset(zero_location)
 
         # Direction of theta
         self.ax.set_theta_direction(-1)
 
     def add_points(self, colour = DEFAULT_COLOUR):
         self.ax.scatter(self.circ.data, [0.5]*len(self.circ.data), color = colour)
+    
+    from numpy import pi
 
-    def add_density(self, colour = DEFAULT_COLOUR, bw_method = 0.05):
+    def add_density(self, colour=DEFAULT_COLOUR, kappa=20, n_bins=500):
         """
-        
-        bw_method:
-            The method used to calculate the estimator bandwidth. 
-            This can be "scott", "silverman", a scalar constant or a callable. If a scalar, this will be used directly as kde.factor. If a callable, it should take a gaussian_kde instance as only parameter and return a scalar. If None (default), "scott" is used.
+        Add a circular density estimate using Von Mises KDE.
+
+        Parameters:
+        - colour: Line color for the density plot.
+        - kappa: Concentration parameter of the Von Mises distribution (like 1/variance).
+        - n_bins: Number of bins to evaluate density on.
         """
 
-        if self.circ.unit == "degrees": #NOTE: from -pi to pi. Maybe find a way to include this information in the circular object??
+        if self.circ.unit == "radians":
             tmp_angles = self.circ.data
-            
-            extended_angles = np.concatenate([tmp_angles + 2 * np.pi, tmp_angles, tmp_angles - 2 * np.pi])
-            print(extended_angles)
 
-            # Density plot using Gaussian KDE
-            density = gaussian_kde(extended_angles, bw_method=bw_method)
-            xs = np.linspace(-2 * np.pi, 2 * np.pi, 2000)  # Extend range # something seems offfffff # data given in degrees not between -pi and pi we need to modulate for plot
-            density_vals = density(xs)
+            if self.circ.zero == 0:
+                min_x, max_x = -pi, pi
+            elif self.circ.zero == "pi":
+                min_x, max_x = 0, 2 * pi
+            else:
+                raise ValueError("Unsupported value for circ.zero. Expected 0 or 'pi'.")
 
-            # Mask to plot only [-pi, pi]
-            mask = (xs >= -np.pi) & (xs <= np.pi)
-            self.ax.plot(xs[mask], density_vals[mask], color=colour, linewidth=1.5)
+            xs, density_vals = vonmises_kde(tmp_angles, kappa, min_x=min_x, max_x=max_x, n_bins=n_bins)
+
+            self.ax.plot(xs, density_vals, color=colour, linewidth=1.5)
 
         else:
             print("upss")
