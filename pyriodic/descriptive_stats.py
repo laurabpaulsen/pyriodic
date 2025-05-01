7a69e8d8875d4ed03de50113132040b1eef04661
laurabpaulsen <202005791@post.au.dk>
2025-05-01 10:34:02 +0200

    init

---
+++ pyriodic/descriptive_stats.py
@@ -0,0 +1,63 @@
+import numpy as np
+from typing import Optional, Union
+
+def circular_mean(
+        rad: np.ndarray,
+        wrap_to_2pi: bool = True
+        ):
+    """
+    Compute the mean angle from a list of angles in radians.
+
+    Parameters:
+    ------------
+    rad : np.ndarray
+        Array of angles in radians.
+
+    wrap_to_2pi : bool, default=True
+        If True, wraps result to range [0, 2π]. If False, result is in [-π, π].
+
+    Returns:
+    --------
+    mean : float
+        The circular mean angle.
+
+    """
+    sin_sum = np.sum(np.sin(rad))
+    cos_sum = np.sum(np.cos(rad))
+
+    mean = np.arctan2(sin_sum, cos_sum)
+
+    if wrap_to_2pi and mean < 0:
+        mean += 2 * np.pi
+    
+    return mean
+
+
+
+def circular_r(
+        rad: np.ndarray
+        ):
+    """
+    Compute the length of the mean resultant vector (r), a measure of circular concentration.
+
+    $$
+     = \sqrt{\bar{C}^2 + \bar{S}^2}
+    $$
+    """
+    sin_sum = np.sum(np.sin(rad))
+    cos_sum = np.sum(np.cos(rad))
+    n = rad.size
+
+    r = np.sqrt(sin_sum**2 + cos_sum**2) / n
+    return r
+
+def angular_deviation(
+        rad: np.ndarray
+        ):
+    raise NotImplementedError
+
+
+def circular_standard_deviation(
+        rad: np.ndarray
+        ):
+    raise NotImplementedError
