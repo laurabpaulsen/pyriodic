945d21e767a81b7e55719992bc35af4bf51218d3
laurabpaulsen <202005791@post.au.dk>
2025-05-02 14:33:28 +0200

    add to circular

--- pyriodic/phase_events.py
+++ pyriodic/phase_events.py
@@ -24,7 +24,10 @@ class PhaseEvents:
     def r(self):
         return {label: circ.r() for label, circ in self.phase_dict.items()}
 
-    def plot(self, savepath: Optional[Union[Path, str]] = None):
+    def plot(
+            self, 
+            histogram: Optional[np.ndarray] = None,
+            savepath: Optional[Union[Path, str]] = None):
         n = len(self.phase_dict)
         cols = int(np.ceil(np.sqrt(n)))
         rows = int(np.ceil(n / cols))
@@ -39,7 +42,7 @@ class PhaseEvents:
         axes = np.array(axes).reshape(-1)  # flatten in case of 2D array
 
         for ax, (label, circ) in zip(axes, self.phase_dict.items()):
-            circ.plot(label=label, ax=ax)
+            circ.plot(label=label, ax=ax, histogram = histogram)
             ax.set_title(f"{label}")
 
         # Hide unused axes
@@ -50,8 +53,20 @@ class PhaseEvents:
 
         if savepath:
             plt.savefig(savepath)
+            plt.close()
         else:
             plt.show()
+
+    def to_circular(self, include: Union[str, list[str]]):
+        if isinstance(include, str):
+            include = [k for k in self.phase_dict if include in k]
+            if not include:
+                raise KeyError(f"No matching labels containing '{include}'")
+
+        matched = [(label, self.phase_dict[label]) for label in include]
+        labels, circulars = zip(*matched)
+        return Circular.from_multiple(circulars, labels=labels)
+    
 
     def __getitem__(self, key):
         return self.phase_dict[key]
