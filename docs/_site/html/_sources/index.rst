c5dec27296a1b0c0197ee9721fdda7d2c83fc5bf
laurabpaulsen <202005791@post.au.dk>
2025-07-04 10:54:13 +0200

    Add GitHub Actions deploy workflow for docs

---
+++ docs/_site/html/_sources/index.rst
@@ -0,0 +1,17 @@
+.. pyriodic documentation master file, created by
+   sphinx-quickstart on Fri Jul  4 10:49:36 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+pyriodic documentation
+======================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
---
+++ docs_src/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'pyriodic'
+copyright = '2025, Laura Bock Paulsen'
+author = 'Laura Bock Paulsen'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+]
+
+html_theme = 'sphinx_book_theme'
+
+html_static_path = ['source/_static']
+
+
