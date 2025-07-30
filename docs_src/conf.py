1f5c71790abe33a113adc2544021ae9939462979
laurabpaulsen <202005791@post.au.dk>
2025-07-30 10:13:33 +0200

    hmm

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -26,7 +26,38 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'pydata_sphinx_theme'
+
+html_theme_options = {
+    "navbar_start": ["navbar-logo"],
+    "navbar_center": ["navbar-nav"],
+    "navbar_end": ["navbar-icon-links"],
+    "navbar_persistent": ["search-button"],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/laurabpaulsen/pyriodic",
+            "icon": "fab fa-github",
+        },
+    ],
+    "navigation_with_keys": True
+    }
+
+
+
+html_theme_options["navbar_links"] = [
+    {"name": "Installation", "url": "installation", "internal": True},
+    {
+        "name": "Tutorials",
+        "url": "tutorials/index",
+        "internal": True,
+    },
+    {
+        "name": "API",
+        "url": "api/index",
+        "internal": True,
+    },
+]
 
 html_static_path = ['source/_static']
 
