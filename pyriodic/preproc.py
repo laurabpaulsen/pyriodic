a3110acfbcead41c6b1bd610da86e57f6a43029c
laurabpaulsen <202005791@post.au.dk>
2025-11-18 10:45:16 +0100

    update distance

--- pyriodic/preproc.py
+++ pyriodic/preproc.py
@@ -292,9 +292,9 @@ class RawSignal:
         peaks : np.ndarray
             Indices of the detected peaks used for phase interpolation.
         """
-        distance = int(distance * self.fs)  # convert seconds to samples
+        
         if peak_finder is None:
-            
+            distance = int(distance * self.fs)  # convert seconds to samples
             peaks = self._peak_finder(self.ts, distance=distance, prominence=prominence)
         else:
             peaks = peak_finder(self.ts)  # assume user handles their own params
