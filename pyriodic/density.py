28068c187022c8429ec036b1e885939428b7b191
laurabpaulsen <202005791@post.au.dk>
2025-11-19 13:46:37 +0100

    add docstring

--- pyriodic/density.py
+++ pyriodic/density.py
@@ -1,14 +1,33 @@
 import numpy as np
-from math import pi
+from scipy.special import i0
 
-def vonmises_kde(data, kappa, min_x=0, max_x=2 * pi, n_bins=100):
-    from scipy.special import i0
+def vonmises_kde(data, kappa, min_x=0, max_x=2*np.pi, n_bins=100):
+    """
+    Vectorized von Mises kernel density estimate for circular data.
 
-    bins = np.linspace(min_x, max_x, n_bins)
+    Parameters
+    ----------
+    data : np.ndarray
+        Circular data in radians.
+    kappa : float
+        Concentration parameter (higher = narrower peaks).
+    min_x : float
+        Minimum value of the evaluation grid (default 0).
+    max_x : float
+        Maximum value of the evaluation grid (default 2*pi).
+    n_bins : int
+        Number of points in the evaluation grid.
+
+    Returns
+    -------
+    x : np.ndarray
+        Evaluation points.
+    kde : np.ndarray
+        Estimated density at each point.
+    """
     x = np.linspace(min_x, max_x, n_bins)
-    # integrate vonmises kernels
-    kde = np.exp(kappa * np.cos(x[:, None] - data[None, :])).sum(1) / (
-        2 * np.pi * i0(kappa)
-    )
-    kde /= np.trapz(kde, x=bins)
-    return bins, kde
+    # Compute vectorized von Mises kernels
+    kde = np.exp(kappa * np.cos(x[:, None] - data[None, :])).sum(axis=1) / (2 * np.pi * i0(kappa))
+    # Normalize
+    kde /= np.trapz(kde, x=x)
+    return x, kde
