ef1c4b89342685120b0c0c093f4b9d0d86b1afdd
laurabpaulsen <202005791@post.au.dk>
2026-07-22 11:05:53 +0200

    bump version

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))  # adjust if needed
 project = 'pyriodic'
 copyright = '2025, Laura Bock Paulsen'
 author = 'Laura Bock Paulsen'
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
